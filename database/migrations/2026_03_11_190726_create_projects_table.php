d7beef89f83086ea1c1f8af0f87ee53ad0de8448
veronicacrisafi <veronicacrisafi3@gmail.com>
2026-03-11 20:13:34 +0100

    creato il modello Project con migrazione

---
+++ database/migrations/2026_03_11_190726_create_projects_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+            $table->string('titolo');
+            $table->string('autore');
+            $table->string('categoria');
+            $table->longText('contenuto');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('projects');
+    }
+};
