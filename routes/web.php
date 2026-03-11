abff2f37ccd2fc9fb8b93db48973f7d0a0439925
veronicacrisafi <veronicacrisafi3@gmail.com>
2026-03-11 21:38:11 +0100

    aggiunta la rotta per la risorsa projects utilizzando il ProjectController

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ProjectController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -25,5 +26,7 @@ Route::middleware(['auth', 'verified'])
         Route::get('/', [DashboardController::class, 'index'])
             ->name('index');
     });
+
+Route::resource('projects', ProjectController::class);
 
 require __DIR__ . '/auth.php';
