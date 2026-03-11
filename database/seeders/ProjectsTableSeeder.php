27dd3566970633046f213ca9c15a0c72079f6549
veronicacrisafi <veronicacrisafi3@gmail.com>
2026-03-11 20:34:20 +0100

    aggiunto il seeder per generare progetti con dati fittizi

--- database/seeders/ProjectsTableSeeder.php
+++ database/seeders/ProjectsTableSeeder.php
@@ -2,16 +2,26 @@
 
 namespace Database\Seeders;
 
+use App\Models\Project;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+
+use Faker\Generator as Faker;
 
 class ProjectsTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(Faker $faker): void
     {
-        //
+        for ($i = 0; $i < 10; $i++) {
+            $newProject = new Project();
+            $newProject->titolo = $faker->sentence();
+            $newProject->autore = $faker->name();
+            $newProject->categoria = $faker->word();
+            $newProject->contenuto = $faker->paragraph();
+            $newProject->save();
+        }
     }
 }
