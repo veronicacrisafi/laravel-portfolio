a44aa5243ddf9a323f5d73e6d92057662e71764d
veronicacrisafi <veronicacrisafi3@gmail.com>
2026-03-11 22:14:30 +0100

    aggiunta la visualizzazione dei progetti nella vista index

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -3,5 +3,19 @@
 @section('title', 'Progetti')
 
 @section('content')
-    ciao
+    <div class="row">
+
+        @foreach ($projects as $project)
+            <div class="col-4 my-3">
+                <div class="card h-100">
+                    <div class="card-body">
+                        <h4 class="card-title mb-5">{{ $project->titolo }}</h4>
+                        <h5 class="card-subtitle mb-4 text-body-secondary">Autore:{{ $project->autore }}</h5>
+                        <h6 class="card-subtitle mb-3">Categoria:{{ $project->categoria }}</h6>
+                        <p class="card-text">{{ $project->contenuto }}</p>
+                    </div>
+                </div>
+            </div>
+        @endforeach
+    </div>
 @endsection
