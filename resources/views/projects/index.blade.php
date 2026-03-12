34aae303522cdfa2b74a1326a60bcd6f1c3bc0de
veronicacrisafi <veronicacrisafi3@gmail.com>
2026-03-12 18:46:20 +0100

    migliorata la struttura della card nella vista index dei progetti per una migliore disposizione degli elementi

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -7,12 +7,16 @@
 
         @foreach ($projects as $project)
             <div class="col-4 my-3">
-                <div class="card h-100">
-                    <div class="card-body">
+                <div class="card h-100 d-flex flex-column">
+                    <div class="card-body d-flex flex-column">
                         <h4 class="card-title mb-5">{{ $project->titolo }}</h4>
                         <h5 class="card-subtitle mb-4 text-body-secondary">Autore: {{ $project->autore }}</h5>
                         <h6 class="card-subtitle mb-3">Categoria: {{ $project->categoria }}</h6>
                         <p class="card-text">{{ $project->contenuto }}</p>
+                        <div class="mt-auto">
+                            <a class='btn btn-outline-primary'
+                                href="{{ route('projects.show', $project->id) }}">Visualizza</a>
+                        </div>
                     </div>
                 </div>
             </div>
