7594eee24bcb13aeefe54e135525e531d4c62f48
veronicacrisafi <veronicacrisafi3@gmail.com>
2026-03-14 10:45:50 +0100

    modificato il metodo destroy nel ProjectController per accettare un'istanza di Project e aggiornato il modulo di eliminazione nella vista show

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -5,12 +5,15 @@
 @section('content')
     <div class="row">
         <div class="col">
-            <div class="d-flex py-4">
-                <a class="btn btn-outline-warning" href="{{ route('projects.edit', $project) }}">Modifica</a>
-                <a class="btn btn-outline-danger" href="{{ route('projects.destroy', $project) }}">Elimina</a>
-
-            </div>
             <div class="card h-100">
+                <div class="d-flex py-4">
+                    <a class="btn btn-outline-warning" href="{{ route('projects.edit', $project) }}">Modifica</a>
+                    <form action="{{ route('projects.destroy', $project) }}" method="POST">
+                        @csrf
+                        @method('DELETE')
+                        <input type="submit" class="btn btn-outline-danger" value="Elimina">
+                    </form>
+                </div>
                 <div class="card-body">
                     <h4 class="card-title mb-5">{{ $project->titolo }}</h4>
                     <h5 class="card-subtitle mb-4 text-body-secondary">Autore: {{ $project->autore }}</h5>
