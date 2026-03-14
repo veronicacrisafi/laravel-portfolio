cc4751448ba91e8972a257c8a8ee5b79dc2d0eda
veronicacrisafi <veronicacrisafi3@gmail.com>
2026-03-14 09:38:23 +0100

    aggiunta la vista per la creazione di nuovi progetti

---
+++ resources/views/projects/create.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.admin')
+
+@section('title', 'Aggiungi un nuovo progetto!')
+
+@section('content')
+    <form action="{{ route('projects.store') }}">
+
+
+
+
+
+    </form>
+
+@endsection
