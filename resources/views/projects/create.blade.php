e48d652c06d419e27d4c1c0e90a1583fa13041b8
veronicacrisafi <veronicacrisafi3@gmail.com>
2026-03-14 09:51:31 +0100

    aggiunto il metodo POST al modulo di creazione progetti e ripristinati i campi del modulo

--- resources/views/projects/create.blade.php
+++ resources/views/projects/create.blade.php
@@ -3,12 +3,24 @@
 @section('title', 'Aggiungi un nuovo progetto!')
 
 @section('content')
-    <form action="{{ route('projects.store') }}">
-
-
-
-
-
+    <form action="{{ route('projects.store') }}" method="POST">
+        <div class="form-control mb-3 d-flex flex-column">
+            <label for="title">Titolo</label>
+            <input type="text" name="title" id="title">
+        </div>
+        <div class="form-control mb-3 d-flex flex-column">
+            <label for="author">Autore</label>
+            <input type="text" name="author" id="author">
+        </div>
+        <div class="form-control mb-3 d-flex flex-column">
+            <label for="category">Categoria</label>
+            <input type="text" name="category" id="category">
+        </div>
+        <div class="form-control mb-3 d-flex flex-column">
+            <label for="content">Contenuto</label>
+            <textarea name="content" id="content" width="100%" rows="5"></textarea>
+        </div>
+        <input type="submit" value="Salva">
     </form>
 
 @endsection
