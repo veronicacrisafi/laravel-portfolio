d4d3e17321163de5786e67372d9081e6bcb9b9cb
veronicacrisafi <veronicacrisafi3@gmail.com>
2026-03-14 10:24:06 +0100

    aggiunto il recupero dei dati del progetto nel modulo di modifica

--- resources/views/projects/edit.blade.php
+++ resources/views/projects/edit.blade.php
@@ -7,19 +7,19 @@
         @csrf
         <div class="form-control mb-3 d-flex flex-column">
             <label for="titolo">Titolo</label>
-            <input type="text" name="titolo" id="titolo">
+            <input type="text" name="titolo" id="titolo" value="{{ $project->titolo }}">
         </div>
         <div class="form-control mb-3 d-flex flex-column">
             <label for="autore">Autore</label>
-            <input type="text" name="autore" id="autore">
+            <input type="text" name="autore" id="autore" value="{{ $project->autore }}">
         </div>
         <div class="form-control mb-3 d-flex flex-column">
             <label for="categoria">Categoria</label>
-            <input type="text" name="categoria" id="categoria">
+            <input type="text" name="categoria" id="categoria" value="{{ $project->categoria }}">
         </div>
         <div class="form-control mb-3 d-flex flex-column">
             <label for="contenuto">Contenuto</label>
-            <textarea name="contenuto" id="contenuto" width="100%" rows="5"></textarea>
+            <textarea name="contenuto" id="contenuto" width="100%" rows="5">{{ $project->contenuto }}</textarea>
         </div>
         <input type="submit" value="Salva">
     </form>
