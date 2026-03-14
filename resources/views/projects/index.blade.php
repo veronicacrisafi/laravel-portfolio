38777de4e9b9578d2dacc10306ba4b3848184f03
veronicacrisafi <veronicacrisafi3@gmail.com>
2026-03-14 11:05:36 +0100

    aggiunti i pulsanti di modifica, eliminazione e crezione del nuovo project nella vista dei progetti

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -3,6 +3,7 @@
 @section('title', 'Progetti')
 
 @section('content')
+    <a href="{{ route('projects.create') }}" class="btn btn-outline-success mb-4">Aggiungi nuovo progetto</a>
     <div class="row">
 
         @foreach ($projects as $project)
@@ -14,8 +15,16 @@
                         <h6 class="card-subtitle mb-3">Categoria: {{ $project->categoria }}</h6>
                         <p class="card-text">{{ $project->contenuto }}</p>
                         <div class="mt-auto">
-                            <a class='btn btn-outline-primary'
+                            <a class='btn btn-outline-primary w-100'
                                 href="{{ route('projects.show', $project->id) }}">Visualizza</a>
+                        </div>
+                        <div class="d-flex py-4 justify-content-between">
+                            <a class="btn btn-outline-warning" href="{{ route('projects.edit', $project) }}">Modifica</a>
+                            <form action="{{ route('projects.destroy', $project) }}" method="POST">
+                                @csrf
+                                @method('DELETE')
+                                <input type="submit" class="btn btn-outline-danger" value="Elimina">
+                            </form>
                         </div>
                     </div>
                 </div>
