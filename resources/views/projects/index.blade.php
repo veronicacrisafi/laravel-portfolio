37241b37df8f7a2abbc876e0174829c9932d52e3
veronicacrisafi <veronicacrisafi3@gmail.com>
2026-03-16 20:48:15 +0100

    aggiornato il template della pagina index per mostrare il tipo del progetto invece della categoria

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -12,7 +12,7 @@
                     <div class="card-body d-flex flex-column">
                         <h4 class="card-title mb-5">{{ $project->titolo }}</h4>
                         <h5 class="card-subtitle mb-4 text-body-secondary">Autore: {{ $project->autore }}</h5>
-                        <h6 class="card-subtitle mb-3">Categoria: {{ $project->categoria }}</h6>
+                        <h6 class="card-subtitle mb-3">Categoria: {{ $project->type->nome }}</h6>
                         <p class="card-text">{{ $project->contenuto }}</p>
                         <div class="mt-auto">
                             <a class='btn btn-outline-primary w-100'
