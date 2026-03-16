b8bbc26d3c7ddf3503ebe0721164ae891e3a1095
veronicacrisafi <veronicacrisafi3@gmail.com>
2026-03-16 18:55:46 +0100

    aggiornato il seeder per la tabella types per includere la generazione di descrizioni casuali e l'assegnazione del nome

--- database/seeders/TypesTableSeeder.php
+++ database/seeders/TypesTableSeeder.php
@@ -2,18 +2,24 @@
 
 namespace Database\Seeders;
 
+use App\Models\Type;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
 
 class TypesTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(Faker $faker): void
     {
         $types = ['Laravel', 'MySQL', 'PHP', 'Internet', 'Videogiochi'];
         foreach ($types as $type) {
+            $newType = new Type();
+            $newType->nome = $type;
+            $newType->descrizione = $faker->sentence();
+            $newType->save();
         }
     }
 }
