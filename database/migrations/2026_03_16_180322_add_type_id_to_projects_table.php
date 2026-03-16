820f64deaab7f903c82eaaaf440bb708c942528c
veronicacrisafi <veronicacrisafi3@gmail.com>
2026-03-16 19:05:15 +0100

    rimossa la colonna 'categoria' e aggiunto il campo 'type_id' alla tabella projects

--- database/migrations/2026_03_16_180322_add_type_id_to_projects_table.php
+++ database/migrations/2026_03_16_180322_add_type_id_to_projects_table.php
@@ -12,7 +12,8 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('projects', function (Blueprint $table) {
-            //
+            $table->dropColumn('categoria');
+            $table->foreignId('type_id')->constrained();
         });
     }
 
