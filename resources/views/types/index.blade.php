6e33c01db6effbdd8de0294cd99aa8eb873d00c9
veronicacrisafi <veronicacrisafi3@gmail.com>
2026-03-17 20:55:39 +0100

    aggiornato il layout della vista index per le tipologie, migliorando la struttura del titolo e dei pulsanti

--- resources/views/types/index.blade.php
+++ resources/views/types/index.blade.php
@@ -8,7 +8,20 @@
             <div class="col-4 my-3">
                 <div class="card h-100 d-flex flex-column">
                     <div class="card-body d-flex flex-column">
-                        <div class="card-title mb-5">{{ $type->nome }}</div>
+                        <div class="card-title text-center my-5">
+                            <h1> {{ $type->nome }}</h1>
+                        </div>
+                        <div class="mt-auto">
+                            <a class='btn btn-outline-primary w-100'
+                                href="{{ route('types.show', $type->id) }}">Visualizza</a>
+                        </div>
+                        <div class="d-flex py-4">
+                            <a class="btn btn-outline-warning me-5 w-50" href="">Modifica</a>
+                            <button type="button" class="btn btn-outline-danger w-50" data-bs-toggle="modal"
+                                data-bs-target="#exampleModal">
+                                Elimina
+                            </button>
+                        </div>
                     </div>
                 </div>
             </div>
