83e9f1352a6bffe229f0139c39de4f77a08549f5
veronicacrisafi <veronicacrisafi3@gmail.com>
2026-03-18 19:02:41 +0100

    aggiunta della relazione molti a molti tra i modelli Project e Technology

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -10,4 +10,9 @@ class Project extends Model
     {
         return $this->belongsTo(Type::class);
     }
+
+    public function technologies()
+    {
+        return $this->belongsToMany(Technology::class);
+    }
 }
--- app/Models/Technology.php
+++ app/Models/Technology.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Technology extends Model
 {
-    //
+    public function projects()
+    {
+        return $this->belongsToMany(Project::class);
+    }
 }
