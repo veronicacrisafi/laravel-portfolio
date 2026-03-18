bd83813a22f771bf57d7fb389e336d2baee98f7a
veronicacrisafi <veronicacrisafi3@gmail.com>
2026-03-18 19:08:11 +0100

    aggiunta delle colonne foreignId per project_id e technology_id nella migrazione della tabella project_technology per la creazione di chiavi esterne nelle migrazioni

--- database/migrations/2026_03_18_180357_create_project_technology_table.php
+++ database/migrations/2026_03_18_180357_create_project_technology_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('project_technology', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('project_id')->constrained();
+            $table->foreignId('technology_id')->constrained();
             $table->timestamps();
         });
     }
